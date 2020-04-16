43f3be63e1b0ada447fe7016924aacc2bda6ff12
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-15 19:58:03 -0600

    side bar menu added and avatars image test

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -3,7 +3,7 @@ export default function() {
     {
       title: 'User Profile',
       htmlBefore: '<i class="material-icons">person</i>',
-      to: '/user-profile-lite',
+      to: '/user-profile',
       htmlAfter: ''
     },
   ];
