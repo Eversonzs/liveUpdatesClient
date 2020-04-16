35820701ac826b6aced9a05717d518655477f42f
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-15 19:47:22 -0600

    adding components for main nav bar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import withTracker from './withTracker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './shards-dashboard/styles/shards-dashboards.1.1.0.min.css';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
