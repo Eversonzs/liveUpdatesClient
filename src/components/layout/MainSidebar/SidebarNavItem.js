48a61104853d1a546a1b7e8bf1e57d2abf7a3639
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-19 13:41:01 -0600

    adding logout function

--- src/components/layout/MainSidebar/SidebarNavItem.js
+++ src/components/layout/MainSidebar/SidebarNavItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink as RouteNavLink } from 'react-router-dom';
 import { NavItem, NavLink } from 'shards-react';
 
 const SidebarNavItem = ({ item }) => (
   <NavItem>
-    <NavLink to={item.to}>
+    <NavLink tag={RouteNavLink} to={item.to}>
       {item.htmlBefore && (
         <div
           className='d-inline-block item-icon-wrapper'
