8f14106765f641e52f098d10e22d4ff6a6bec6c0
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-18 22:53:09 -0600

    adding state on user-profile view and adding redirect if is not authenticated

--- src/views/UserProfileLite.js
+++ src/views/UserProfileLite.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import { Container, Row, Col } from 'shards-react';
+import { Redirect } from 'react-router-dom';
 
 import PageTitle from '../components/common/PageTitle';
 import UserDetails from '../components/user-profile-lite/UserDetails';
 import UserAccountDetails from '../components/user-profile-lite/UserAccountDetails';
 
-const UserProfileLite = () => (
-  <Container fluid className='main-content-container px-4'>
-    <Row noGutters className='page-header py-4'>
-      <PageTitle title='User Profile' subtitle='Overview' md='12' className='ml-sm-auto mr-sm-auto' />
-    </Row>
-    <Row>
-      <Col lg='4'>
-        <UserDetails />
-      </Col>
-      <Col lg='8'>
-        <UserAccountDetails />
-      </Col>
-    </Row>
-  </Container>
-);
+class UserProfileLite extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+    };
+  }
+
+  render() {
+    const { isAuthenticated } = this.state;
+    return (
+      isAuthenticated ?
+      (
+        <Container fluid className='main-content-container px-4'>
+          <Row noGutters className='page-header py-4'>
+            <PageTitle title='User Profile' subtitle='Overview' md='12' className='ml-sm-auto mr-sm-auto' />
+          </Row>
+          <Row>
+            <Col lg='4'>
+              <UserDetails />
+            </Col>
+            <Col lg='8'>
+              <UserAccountDetails />
+            </Col>
+          </Row>
+        </Container>
+      ) :
+      <Redirect to='/login'/>
+    )
+  };
+};
 
 export default UserProfileLite;
