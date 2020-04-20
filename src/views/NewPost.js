de72c355bfeae108319405b4012054f8277ac413
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-19 18:18:19 -0600

    adding Editor component for new post

---
+++ src/views/NewPost.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Row, Col } from 'shards-react';
+
+import PageTitle from '../components/common/PageTitle';
+import Editor from '../components/new-post/Editor';
+
+const AddNewPost = () => (
+  <Container fluid className='main-content-container px-4 pb-4'>
+    {/* Page Header */}
+    <Row noGutters className='page-header py-4'>
+      <PageTitle sm='4' title='Add New Post' subtitle='Live-Updates' className='text-sm-left' />
+    </Row>
+
+    <Row>
+      {/* Editor */}
+      <Col lg='9' md='12'>
+        <Editor />
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default AddNewPost;
