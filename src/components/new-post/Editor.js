2353f6179a5b899020340b14401508d2e61a06d0
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-19 18:43:28 -0600

    adding button to send post and function on props

--- src/components/new-post/Editor.js
+++ src/components/new-post/Editor.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
-import { Card, CardBody, Form, FormInput } from 'shards-react';
+import {
+    Card,
+    CardBody,
+    Form,
+    FormInput,
+    Row,
+    Button,
+} from 'shards-react';
 
 import 'react-quill/dist/quill.snow.css';
 import './Editor.css';
 
-const Editor = () => (
-  <Card small className='mb-3'>
-    <CardBody>
-      <Form className='add-new-post'>
-        <FormInput size='lg' className='mb-3' placeholder='Your Post Title' />
-        <ReactQuill className='add-new-post__editor mb-1' />
-      </Form>
-    </CardBody>
-  </Card>
-);
+const Editor = (props) => {
+  const { createPost } = props;
+  return (
+    <Card small className='mb-3'>
+      <CardBody>
+        <Form className='add-new-post'>
+            <FormInput size='lg' className='mb-3' placeholder='Your Post Title' />
+            <ReactQuill className='add-new-post__editor mb-1' />
+        </Form>
+      </CardBody>
+      <Row className='col float-right'>
+        <Button
+          onClick={() => createPost()}
+          className='ml-auto'
+        >
+          Send
+        </Button>
+      </Row>
+      <br></br>
+    </Card>
+  )
+
+}
 
 export default Editor;
