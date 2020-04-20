634de80c97f03a43fdb321e6bc4281731ec297fb
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-19 18:36:03 -0600

    clean code on new post component

--- src/views/NewPost.js
+++ src/views/NewPost.js
@@ -5,29 +5,29 @@ import PageTitle from '../components/common/PageTitle';
 import Editor from '../components/new-post/Editor';
 
 class NewPost extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
-
-    render() {
-        return (
-            <Container fluid className='main-content-container px-4 pb-4'>
-                {/* Page Header */}
-                <Row noGutters className='page-header py-4'>
-                    <PageTitle sm='4' title='Add New Post' subtitle='Live-Updates' className='text-sm-left' />
-                </Row>
-
-                <Row>
-                    {/* Editor */}
-                    <Col lg='9' md='12'>
-                        <Editor />
-                    </Col>
-                </Row>
-            </Container>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
     };
+  }
+
+  render() {
+    return (
+      <Container fluid className='main-content-container px-4 pb-4'>
+        {/* Page Header */}
+        <Row noGutters className='page-header py-4'>
+          <PageTitle sm='4' title='Add New Post' subtitle='Live-Updates' className='text-sm-left' />
+        </Row>
+
+        <Row>
+          {/* Editor */}
+          <Col lg='9' md='12'>
+            <Editor />
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
 };
 
 export default NewPost;
